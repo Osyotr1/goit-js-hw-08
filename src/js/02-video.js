1547b896b9cb75d3f7c06e5a19c2845a60574e27
Volodymyr Horin <vova.gorin08@gmail.com>
2022-06-10 18:06:48 +0300

    vimeo done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
+
+const player = new Player('vimeo-player');
+
+const CURRENTTIMEKEY = "videoplayer-current-time";
+
+let playTime = 0;
+let currentTime = 0;
+
+function onTimeUpdate(data) {
+    playTime = data.seconds
+    localStorage.setItem(CURRENTTIMEKEY, playTime)
+    
+};
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+currentTime = localStorage.getItem(CURRENTTIMEKEY);
+
+player.setCurrentTime(currentTime).then(function (seconds) {
+    
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            
+            break;
+
+        default:
+            
+            break;
+    }
+});
 
